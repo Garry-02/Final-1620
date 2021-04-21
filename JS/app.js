3152e0e0aa98f2986a26d5971574f4533d33b21f
Garry <gurjyotsingh76@gmail.com>
2021-04-22 02:19:54 +0530

    adding the image dynamically using JS

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+const albums = [
+    { name: "Alumb 1", price: 12.99, inCart: false},
+    { name: "Alumb 2", price: 14.99, inCart: false},
+    { name: "Alumb 3", price: 9.99, inCart: false},
+    { name: "Alumb 4", price: 19.99, inCart: false}
+];
+
+album1 = document.getElementById("Album 1").src = "Images/Album1.png";
+album2 = document.getElementById("Album 2").src = "Images/Album2.png";
+album3 = document.getElementById("Album 3").src = "Images/Album3.png";
+album4 = document.getElementById("Album 4").src = "Images/Album4.png";
